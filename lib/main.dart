c9b275304a27d3f2e2ff9bf87f5d8d44f77ab4b9
LEVONO <kcdbordios.work@gmail.com>
2025-07-12 17:15:31 +0800

    Added separate file for screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
+import 'package:advanced_calculator/screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(AdvancedCalculator());
 
 class AdvancedCalculator extends StatelessWidget {
+  const AdvancedCalculator({super.key});
+
   @override
   Widget build(BuildContext context) => MaterialApp(
     title: 'Advanced Calculator',
     home: Screen(),
     debugShowCheckedModeBanner: false,
   );
-}
-
-class Screen extends StatefulWidget {
-  @override
-  _ScreenState createState() => _ScreenState();
-}
-class _ScreenState extends State<Screen> {
-  String display = '';
-
-  @override
-  Widget build(BuildContext context) => Scaffold();
 }
---
+++ lib/screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Screen extends StatefulWidget {
+  const Screen({super.key});
+
+  @override
+  ScreenState createState() => ScreenState();
+}
+class ScreenState extends State<Screen> {
+  String display = '';
+
+  @override
+  Widget build(BuildContext context) => Scaffold();
+}
