ffe13e8aa7074c2e50e0f76cf64386f593d6094a
LEVONO <kcdbordios.work@gmail.com>
2025-07-12 17:05:46 +0800

    add

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AdvancedCalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Advanced Calculator',
+    debugShowCheckedModeBanner: false,
+  );
+}
